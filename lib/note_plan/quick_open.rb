0509dfdf9729986ca8cbaa6f326aa47d8299f743
Mark Beattie <beattie.mark@gmail.com>
2020-03-03 16:25:22 +1100

    14 - Quick open action includes calendar notes

--- lib/note_plan/quick_open.rb
+++ lib/note_plan/quick_open.rb
@@ -13,13 +13,23 @@ module NotePlan
     def alfred_items
       [].tap do |array|
         for_each_note do |note_file|
-          array << Alfred::Item.new(
-            note_file.heading,
-            subtitle: note_file.basename,
-            arg: note_file.heading
-          )
+          array << alfred_item_for(note_file)
+        end
+
+        for_each_calendar_entry do |note_file|
+          array << alfred_item_for(note_file)
         end
       end.uniq.sort_by(&:title)
+    end
+
+    private
+
+    def alfred_item_for(note_file)
+      Alfred::Item.new(
+        note_file.heading,
+        subtitle: note_file.basename,
+        arg: note_file.heading
+      )
     end
   end
 end
